e38e7700016e5970f1c9194de4a341a6de7ef4b8
Lucas Carvalho Silva <154228622+Luckaz7@users.noreply.github.com>
2025-02-27 13:52:53 -0300

    Atualizar o estruturaAPI.py

--- MinimalAPI_Python/estruturaAPI.py
+++ MinimalAPI_Python/estruturaAPI.py
@@ -1,1 +1,58 @@
-estruturaAPI.py
+from flask import Flask, request, jsonify
+import fitz
+import requests
+
+app = Flask(__name__)
+
+API_URL = "https://api.groq.com/openai/v1/chat/completions"
+API_KEY = "SUA API KEY"
+MODEL = "llama-3.3-70b-versatile"
+
+
+def extrairTextoPdf(arquivoPdf):
+    documentoPdf = fitz.open(stream=arquivoPdf.read(), filetype="pdf")
+    texto = ""
+    for pagina in documentoPdf:
+        texto = pagina.get_text()
+    return texto
+
+
+def chamandoApi(texto):
+    cabecalho = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    requisicao = {
+        "model": MODEL,
+        "messages": [
+            {"role": "system",
+                "content": "Extraia as seguintes informações do texto: número do processo e partes envolvidas(autor, réu, advogados)."},
+            {"role": "user", "content": texto}
+        ]
+    }
+
+    resposta = requests.post(API_URL, headers=cabecalho, json=requisicao)
+    return resposta.json()
+
+
+@app.route("/extract", methods=["POST"])
+def extrair():
+    if 'file' not in request.files:
+        return jsonify({"error": "Nenhum arquivo enviado."}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "Nome de arquivo inválido."}), 400
+
+    try:
+        texto = extrairTextoPdf(file)
+        resposta_llm = chamandoApi(texto)
+        return jsonify(resposta_llm), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
